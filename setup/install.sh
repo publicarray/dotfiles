bda61152a42377e994339b4b1f1f81628d682459
Sebastian Schmidt <publicarray@icloud.com>
2015-05-29 16:32:58 +1000

    random fortune spoken from cowsay in your shell (optional)
    + Removed: autocomplete-plus and autocomplete-css. Atom now preinstalls
    them so there is no need to install them again.
    + New: update atom packages with update alias
    + Improved: gitconfig
    + spell fix

--- setup/install.sh
+++ setup/install.sh
@@ -187,11 +187,11 @@ info "\nIt is recommended to them in order:"
                 # Install atom.io packages
                 if ask "Do you want to install Atom packages? (requires Atom Shell Commands)" Y; then
                     if [[ "$(type -P apm)" ]]; then
-                        heading "Installing Sublime Packages and Themes"
+                        heading "Installing Atom Packages and Themes"
                         # see https://atom.io/packages/linter for more linters
                         apm install atom-soda-dark-ui monokai file-icons color-picker dash atom-beautify linter linter-php linter-javac linter-jshint linter-clang linter-csslint linter-htmlhint highlight-selected
                         # see https://github.com/atom-community/autocomplete-plus/wiki/Autocomplete-Providers for more providers
-                        apm install autocomplete-plus autocomplete-plus autocomplete-css autocomplete-hack
+                        apm install autocomplete-hack
                         # optional:
                         # apm install git-control
                         # apm install emmet - slows down start-up a bit >100ms
