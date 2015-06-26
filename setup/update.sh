31fe3c205555fece9cdbfe9795c1c790bf9863f7
Sebastian Schmidt <publicarray@icloud.com>
2015-06-26 21:07:40 +1000

    prevent git warning when the usershell is modified

--- setup/update.sh
+++ setup/update.sh
@@ -3,8 +3,10 @@
 DOTFILES=~/.dotfiles
 source "$DOTFILES/setup/functions.sh"
 
-cd ~/.zprezto;
-git pull origin master;
+cd ~/.zprezto
+git pull origin master
 
-cd "$DOTFILES";
-git pull origin master;
+cd "$DOTFILES"
+# don't overwrite user configurations in the usershell file
+git update-index --assume-unchanged symlink/usershell
+git pull origin master
