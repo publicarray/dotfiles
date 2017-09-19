9af3f758951e2578850cfc5230cd4b101f4a593d
Sebastian Schmidt <publicarray@icloud.com>
2017-09-20 02:29:17 +1000

    update firefox user.js and extention

--- apps/firefox/myuser.js
+++ apps/firefox/myuser.js
@@ -20,6 +20,16 @@ user_pref("services.sync.engine.tabs", false);
  * ghacks-user.js - Modifications                                             *
  * https://github.com/ghacksuserjs/ghacks-user.js                             *
  ******************************************************************************/
+/* 0001: start Firefox in PB (Private Browsing) mode
+ * [SETTING] Options>Privacy>History>Custom Settings>Always use private browsing mode
+ * [NOTE] In this mode *all* windows are "private windows" and the PB mode icon is not displayed
+ * [NOTE] The P in PB mode is misleading: it means no "persistent" local storage of history,
+ * caches, searches or cookies (which you can achieve in normal mode). In fact, it limits or
+ * removes the ability to control these, and you need to quit Firefox to clear them. PB is best
+ * used as a one off window (File>New Private Window) to provide a temporary self-contained
+ * new instance. Closing all Private Windows clears all traces. Repeat as required.
+ * [1] https://wiki.mozilla.org/Private_Browsing ***/
+   // user_pref("browser.privatebrowsing.autostart", true);
 
 // 0301: browser auto update
 user_pref("app.update.enabled", true);
@@ -34,7 +44,7 @@ user_pref("extensions.update.enabled", true);
 user_pref("extensions.update.autoUpdateDefault", true);
 
 // 0380:
-user_pref("services.sync.enabled", true); // (hidden pref)
+// user_pref("services.sync.enabled", true); // (hidden pref)
 
 // https://developer.mozilla.org/en-US/docs/Mozilla/Preferences/Preference_reference/dom.event.clipboardevents.enabled
 // [This has a security / convenience trade-off. Set to false for better security and
@@ -62,31 +72,22 @@ user_pref("browser.safebrowsing.phishing.enabled", true); // (FF50+)
    // this covers malware and PUPs (potentially unwanted programs)
 user_pref("browser.safebrowsing.downloads.enabled", true);
    // disable "Warn me about unwanted and uncommon software" Also under Options>Security (FF48+)
-user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);
-user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+// user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);
+// user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
    // yet more prefs added (FF49+)
-user_pref("browser.safebrowsing.downloads.remote.block_dangerous", true);
-user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", true);
+// user_pref("browser.safebrowsing.downloads.remote.block_dangerous", true);
+// user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", true);
 // 0410c: disable Google safebrowsing downloads, updates
-user_pref("browser.safebrowsing.provider.google.updateURL", "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_API_KEY%"); // update google lists
-user_pref("browser.safebrowsing.provider.google.gethashURL", "https://shavar.services.mozilla.com/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // list hash check
-user_pref("browser.safebrowsing.provider.google4.updateURL", "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_API_KEY%"); // (FF50+)
+// user_pref("browser.safebrowsing.provider.google.updateURL", "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_API_KEY%"); // update google lists
+// user_pref("browser.safebrowsing.provider.google.gethashURL", "https://shavar.services.mozilla.com/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // list hash check
+// user_pref("browser.safebrowsing.provider.google4.updateURL", "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_API_KEY%"); // (FF50+)
 // user_pref("browser.safebrowsing.provider.google4.gethashURL", ""); // (FF50+)
 // 0410d: disable mozilla safebrowsing downloads, updates
    // NOTE: These two prefs are also used for Tracking Protection (see 0420)
-user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "https://shavar.services.mozilla.com/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // resolves hash conflicts
-user_pref("browser.safebrowsing.provider.mozilla.updateURL", "https://shavar.services.mozilla.com/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // update FF lists
+// user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "https://shavar.services.mozilla.com/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // resolves hash conflicts
+// user_pref("browser.safebrowsing.provider.mozilla.updateURL", "https://shavar.services.mozilla.com/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2"); // update FF lists
 
 
-// 0812: disable search and form history
-   // Under Options>Privacy> if you set Firefox to "use custom settings" there will be a
-   // setting called "remember search and form history".
-   // You can clear formdata on exiting Firefox (see 2803)
-   user_pref("browser.formfill.enable", false);
-// 0813: disable saving form data on secure websites - PRIVACY (shoulder surfers etc)
-   // For convenience & functionality, this is best left at default true.
-   // You can clear formdata on exiting Firefox (see 2803)
-   user_pref("browser.formfill.saveHttpsForms", false);
 // 0816: disable browsing and download history
    // Under Options>Privacy> if you set Firefox to "use custom settings" there will be a
    // setting called "remember my browsing and download history"
@@ -126,7 +127,7 @@ user_pref("gfx.font_rendering.opentype_svg.enabled", true); // [fix Firefox tab 
 // 1405: disable woff2
 // [use uBlock for more control: https://github.com/gorhill/uBlock/wiki/Per-site-switches#no-remote-fonts]
 user_pref("gfx.downloadable_fonts.woff2.enabled", true); // [Why not?]
-// 1603: referer, WHEN to send
+// 1603: referer, WHEN to send [use SmartReferer Extension]
    // [http://kb.mozillazine.org/Network.http.sendRefererHeader]
    // [https://github.com/ghacksuserjs/ghacks-user.js/issues/5#issuecomment-280867769]
    // 0=never, 1=send only when links are clicked, 2=for links and images (default)
@@ -203,70 +204,13 @@ user_pref("network.http.spdy.enabled.http2", true); // [sorry, but I <3 http/2]
    // they treat them as severe/critical and 4. for convenience
 user_pref("pdfjs.disabled", false); // [I'm 50:50 with this. I think pdf.js has less of chance of infecting the machine than the preview.app]
 
-/*** 2697: USER AGENT (UA) SPOOFING
-     Spoofing your UA to *LOWER* entropy *does* *not* *work*. It may even cause site breakage
-     depending on your values. Even if you spoof, like TBB (Tor Browser Bundle) does, as the
-     latest ESR, it still *does* *not* *work*. There are two main reasons for this.
-       1. Many of the components that make up your UA can be derived by other means. And when
-          those values differ, you provide more bits and raise entropy. Examples of leaks include
-          navigator objects, resource://URIs, <isindex> locale, feature detection and more.
-       2. You are not in a controlled set of significant numbers, where the values are enforced
-          by default. It works for TBB because for TBB, the spoofed values ARE their default.
-     * We do not recommend UA spoofing yourself, leave it to privacy.resistFingerprinting (see 2699)
-     * Values below are for example only based on the current ESR/TBB at the time of writing
-***/
-/* 2697a: navigator.userAgent leaks in JS
- * [NOTE] setting this will break any UA spoofing add-on whitelisting ***/
-    // user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0); // (hidden pref)
-                                        // Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0 [Tor]
-                                        // Mozilla/5.0 (Windows NT 6.1; rv:54.0) Gecko/20100101 Firefox/54.0 [current FF]
-                                        // Mozilla/5.0 (Windows NT 6.1; rv:50.0) Gecko/20100101 Firefox/50.0
-                                        // Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0 [Windows 10, 64bit, v51]
-/* 2697b: navigator.buildID (see gecko.buildID in about:config) reveals build time
- * down to the second which defeats user agent spoofing and can compromise OS etc
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=583181 ***/
-   // user_pref("general.buildID.override", "20100101"); // (hidden pref)
-/* 2697c: navigator.appName ***/
-   // user_pref("general.appname.override", "Netscape"); // (hidden pref)
-/* 2697d: navigator.appVersion ***/
-   // user_pref("general.appversion.override", "5.0 (Windows)"); // (hidden pref)
-/* 2697e: navigator.platform leaks in JS ***/
-   // user_pref("general.platform.override", "Win32"); // (hidden pref)
-/* 2697f: navigator.oscpu leaks in JS ***/
-   // user_pref("general.oscpu.override", "Windows NT 6.1"); // (hidden pref)
-/* 2697g: also see 0204 for general.useragent.locale ***/
-
-/*** 2698: FIRST PARTY ISOLATION (FPI) [Not ready yet] ***/
-/* 2698a: enable first party isolation pref and OriginAttribute (FF51+)
- * [WARNING] breaks lots of cross-domain logins and site functionality until perfected
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1260931 ***/
-/* 2698b: isolate favicons (FF52+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1277803 ***/
-/* 2698c: isolate OCSP cache (FF52+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1264562 ***/
-/* 2698d: isolate Shared Workers (FF52+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1268726 ***/
-/* 2698e: isolate SSL session cache (FF52+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1316283 ***/
-/* 2698f: isolate media cache (FF53+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1317927 ***/
-/* 2698g: isolate HSTS and HPKP (FF54+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1323644 ***/
-/* 2698h: isolate HTTP Alternative Services (FF54+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1334690 ***/
-/* 2698i: isolate SPDY/HTTP2 (FF55+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1334693 ***/
-/* 2698j: isolate DNS cache (FF55+)
- * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1337893 ***/
-   // user_pref("privacy.firstparty.isolate", true);  // [let's test if it blows up in my face]
-   // user_pref("privacy.firstparty.isolate.restrict_opener_access", true); // (FF54+)
-// 2701: disable cookies on all sites
+// 2701: disable cookies on all sites [breaks many websites, use cookie auto-delete extension]
    // you can set exceptions under site permissions or use an extension (eg Cookie Controller)
    // http://kb.mozillazine.org/Network.cookie.cookieBehavior
    // 0=allow all 1=allow same host 2=disallow all 3=allow 3rd party if it already set a cookie
    // [2 is too restrictive, fix: twitter login. Setting also impacts local storage and indexedDB.
    // The problem was that that selectively allowing cookies for Twitter did not allow indexedDB. Twitter needs indexedDB for login]
-// user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.cookie.cookieBehavior", 1);
 // 2706: disable Storage API (FF51+) which gives sites' code the ability to find out how much space
    // they can use, how much they are already using, and even control whether or not they need to
    // be alerted before the user agent disposes of site data in order to make room for other things.
@@ -285,8 +229,8 @@ user_pref("pdfjs.disabled", false); // [I'm 50:50 with this. I think pdf.js has 
  * The override values are a starting point to round from if you want some control
  * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1330882
  * [2] https://metrics.mozilla.com/firefox-hardware-report/ ***/
-   user_pref("privacy.window.maxInnerWidth", 1200); // (hidden pref)
-   user_pref("privacy.window.maxInnerHeight", 700); // (hidden pref)
+   // user_pref("privacy.window.maxInnerWidth", 1200); // (hidden pref)
+   // user_pref("privacy.window.maxInnerHeight", 700); // (hidden pref)
 
 
 // 3000: PERSONAL SETTINGS //
@@ -300,7 +244,7 @@ user_pref("layout.spellcheckDefault", 2); // [the more spell checking the better
 
 /* 3020: FYI: urlbar click behaviour (with defaults) ***/
 user_pref("browser.urlbar.clickSelectsAll", false);
-user_pref("browser.urlbar.doubleClickSelectsAll", true);
+user_pref("browser.urlbar.doubleClickSelectsAll", false);
 /******************************************************************************
  * user.js - Additions                                                        *
  * https://github.com/pyllyukko/user.js                                       *
@@ -317,7 +261,7 @@ user_pref("devtools.debugger.force-local",          true);
 // user_pref("security.csp.enable",                true);
 
 // https://bugzilla.mozilla.org/show_bug.cgi?id=855326 [personal preference]
-user_pref("security.csp.experimentalEnabled",           true);
+// user_pref("security.csp.experimentalEnabled",           true);
 
 // CIPHERS
 // [most recent version of FF will have reasonable secure ciphers by default]
@@ -339,6 +283,8 @@ user_pref("browser.newtab.preload", true);
 user_pref("browser.feeds.showFirstRunUI", false);
 user_pref("browser.reader.detectedFirstArticle", true);
 user_pref("browser.customizemode.tip0.shown", true);
+// [disable reader]
+user_pref("reader.parse-on-load.enabled", false);
 
 // [Java plugin state - never activate]
 user_pref("plugin.state.java", 0);
@@ -362,7 +308,7 @@ user_pref("browser.download.useDownloadDir", true);
 
 // [WebGL]
 // user_pref("webgl.disabled", false);
-user_pref("pdfjs.enableWebGL", true);
+user_pref("pdfjs.enableWebGL", true); // [lower CPU usage]
 // user_pref("webgl.min_capability_mode", false);
 // user_pref("webgl.disable-extensions", true);
 // user_pref("webgl.enable-draft-extensions", true);
@@ -376,8 +322,6 @@ user_pref("pdfjs.enableWebGL", true);
 
 // [js contextmenu events]
 // user_pref("dom.event.contextmenu.enabled", false);
-// [disable reader]
-user_pref("reader.parse-on-load.enabled", false);
 
 // [GeoLocation (google maps) - risk of physical security]
 // http://www.mozilla.org/en-US/firefox/geolocation/
@@ -398,4 +342,4 @@ user_pref("layout.css.servo.enabled", true);
 
 user_pref("network.dns.disablePrefetch", false); // I run my own non logging DNS resolver with dnscrypt
 
-user_pref("ghacks_user.js.parrot", "9999 syntax error: The parrot is alive! Nope. I lied. The parrot is in heaven.");
+user_pref("ghacks_user.js.parrot", "9090909 syntax error: The parrot is alive! Nope. I lied. The parrot is in heaven.");
--- apps/firefox/readme.md
+++ apps/firefox/readme.md
@@ -2,30 +2,34 @@
 
 [uBlock Origin](https://github.com/gorhill/uBlock) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/)
 
-[LastPass](https://lastpass.com/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/) or any other password manager
+[Decentraleyes](https://decentraleyes.org/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/decentraleyes/)
 
-[SSleuth](https://github.com/sibiantony/ssleuth/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/ssleuth/)
-
-[Self-Destructing Cookies](https://addons.mozilla.org/en-US/firefox/addon/self-destructing-cookies/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/self-destructing-cookies/)
+[1password](https://1password.com/downloads/) [Get the Extension](https://agilebits.com/onepassword/extensions) / [LastPass](https://lastpass.com/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/) or any other password manager
 
 [HTTPS Everywhere](https://www.eff.org/https-everywhere) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/https-everywhere/)
 
-[CanvasBlocker](https://github.com/kkapsner/CanvasBlocker/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/canvasblocker/)
-
-[No resource URI leak](https://notabug.org/desktopd/no-resource-uri-leak) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/no-resource-uri-leak/) - Test: https://browserleaks.com/firefox
-
-[Pure-Url](http://veg.by/en/blog/category/firefox/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/pure-url/)
-
-Don't touch my tabs! (rel=noopener) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/dont-touch-my-tabs/) - Q: why? A: read https://mathiasbynens.github.io/rel-noopener/
+[Cookie-AutoDelete](https://github.com/Cookie-AutoDelete/Cookie-AutoDelete) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/cookie-autodelete/)
 
 [Smart Referer](https://github.com/meh/smart-referer) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/smart-referer/)
 
-## Additional extensions
+[Canvas Defender](https://multiloginapp.com/canvasdefender-browser-extension/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/no-canvas-fingerprinting/) - [Test on browserleaks.com](http://www.browserleaks.com/canvas)
 
-[Beyond Australis](https://github.com/Quicksaver/The-Fox--Only-Better) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/the-fox-only-better/) or [Omnibar](https://addons.mozilla.org/en-US/firefox/addon/omnibar/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/omnibar/)
+[Neat URL](https://github.com/Smile4ever/firefoxaddons) - [Get the Extension](https://addons.mozilla.org/firefox/addon/neat-url/)
 
-[Profile Switcher](https://freeshell.de/~kaosmos/profileswitcher-en.html) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/profileswitcher/)
+[Don't touch my tabs! (rel=noopener)](https://mathiasbynens.github.io/rel-noopener/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/dont-touch-my-tabs/)
 
-[Random Agent Spoofer](https://github.com/dillbyrne/random-agent-spoofer) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/random-agent-spoofer/)
+## Additional Extensions
 
-[Disable IndexdDB](https://github.com/ChrisAntaki/disable-indexeddb) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/disable-indexeddb/)
+[uBO-Scope](https://github.com/gorhill/uBO-Scope) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/ubo-scope/)
+
+[uMatrix](https://github.com/gorhill/uMatrix) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/umatrix/)
+
+[Request Control](https://github.com/tumpio/requestcontrol) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/requestcontrol/) - [Rules](https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.5-Request-Control)
+
+<!-- [Violentmonkey](https://violentmonkey.github.io/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/) or [Tampermonkey][https://tampermonkey.net/] - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) -->
+
+<!-- and install https://greasyfork.org/en/scripts/735-anti-adblock-killer-reek  andd https://github.com/ghacksuserjs/ghacks-user.js/wiki/3.1-User-Scripts-->
+
+<!-- [SSleuth](https://github.com/sibiantony/ssleuth/) - [Get the Extension](https://addons.mozilla.org/en-US/firefox/addon/ssleuth/) -->
+
+## [Also see ghacks-user.js/wiki/Appendix-B:-Extensions](https://github.com/ghacksuserjs/ghacks-user.js/wiki/Appendix-B:-Extensions)
